7318bf4449fdd3801445941841f4b6701f893d4a
Mattis Kieffer <mattis.kieffer@hotmail.de>
2024-12-05 21:29:47 +0100

    use async_trait for bt-model

--- src/controller/bluetooth.rs
+++ src/controller/bluetooth.rs
@@ -8,6 +8,7 @@ use crate::core::events::BluetoothEvent;
 use crate::model::bluetooth::{BluetoothModelApi, DeviceDescriptor, HeartrateMessage};
 use crate::model::storage::ModelHandle;
 use crate::{core::events::AppEvent, model::bluetooth::AdapterDescriptor};
+use async_trait::async_trait;
 use btleplug::api::{Peripheral, ScanFilter};
 use btleplug::{
     api::{BDAddr, Central, Manager as _},
@@ -18,8 +19,6 @@ use anyhow::{anyhow, Result};
 use egui::ahash::HashMap;
 use futures::StreamExt;
 use log::{trace, warn};
-use std::future::Future;
-use std::pin::Pin;
 use std::sync::Arc;
 use tokio::sync::broadcast::Sender;
 use tokio::sync::RwLock;
@@ -27,38 +26,31 @@ use tokio::task::JoinHandle;
 use uuid::Uuid;
 
 /// API for Bluetooth operations.
+#[async_trait]
 pub trait BluetoothApi: Send + Sync {
     /// Returns a reference to the Bluetooth model.
     fn get_model(&self) -> Result<ModelHandle<dyn BluetoothModelApi>>;
 
     /// Discovers available Bluetooth adapters.
-    fn discover_adapters<'a>(&'a mut self)
-        -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>>;
+    async fn discover_adapters(&mut self) -> Result<()>;
 
     /// Selects a Bluetooth adapter.
-    fn select_adapter<'a>(
-        &'a self,
-        uuid: &'a AdapterDescriptor,
-    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>>;
+    async fn select_adapter(&self, uuid: &AdapterDescriptor) -> Result<()>;
 
     /// Selects a Bluetooth peripheral.
-    fn select_peripheral<'a>(
-        &'a self,
-        uuid: &'a DeviceDescriptor,
-    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>>;
+    async fn select_peripheral(&self, uuid: &DeviceDescriptor) -> Result<()>;
 
     /// Starts scanning for peripherals.
-    fn start_scan<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>>;
+    async fn start_scan(&mut self) -> Result<()>;
 
     /// Stops scanning for peripherals.
-    #[allow(dead_code)]
-    fn stop_scan<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>>;
+    async fn stop_scan(&mut self) -> Result<()>;
 
     /// Starts listening for notifications from a specific peripheral.
-    fn start_listening<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>>;
+    async fn start_listening(&mut self) -> Result<()>;
 
     /// Stops listening for notifications from peripherals.
-    fn stop_listening<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>>;
+    async fn stop_listening(&mut self) -> Result<()>;
 }
 
 /// The Bluetooth Controller manages BLE interactions, including scanning for devices
@@ -118,49 +110,46 @@ impl BluetoothController {
     ///
     /// # Returns
     /// A future that resolves to a join handle for the listener task.
-    #[allow(clippy::type_complexity)]
-    fn listen_to_peripheral<'a>(
+    async fn listen_to_peripheral(
         adapter: Adapter,
         peripheral_address: BDAddr,
         tx: Sender<AppEvent>,
-    ) -> Pin<Box<dyn Future<Output = Result<JoinHandle<Result<()>>>> + Send + 'a>> {
-        Box::pin(async move {
-            let peripherals = adapter.peripherals().await?;
-            let cheststrap = peripherals
-                .into_iter()
-                .find(|p| p.address() == peripheral_address)
-                .ok_or(anyhow!("Peripheral not found"))?;
-            cheststrap.connect().await?;
+    ) -> Result<JoinHandle<Result<()>>> {
+        let peripherals = adapter.peripherals().await?;
+        let cheststrap = peripherals
+            .into_iter()
+            .find(|p| p.address() == peripheral_address)
+            .ok_or(anyhow!("Peripheral not found"))?;
+        cheststrap.connect().await?;
 
-            cheststrap.discover_services().await?;
+        cheststrap.discover_services().await?;
 
-            let char = cheststrap
-                .characteristics()
-                .iter()
-                .find(|c| c.uuid == HEARTRATE_MEASUREMENT_UUID)
-                .ok_or(anyhow!("Peripheral has no Heartrate attribute"))?
-                .clone();
+        let char = cheststrap
+            .characteristics()
+            .iter()
+            .find(|c| c.uuid == HEARTRATE_MEASUREMENT_UUID)
+            .ok_or(anyhow!("Peripheral has no Heartrate attribute"))?
+            .clone();
 
-            cheststrap.subscribe(&char).await?;
+        cheststrap.subscribe(&char).await?;
 
-            let mut notification_stream = cheststrap.notifications().await?;
-            let fut = tokio::spawn(async move {
-                while let Some(data) = notification_stream.next().await {
-                    if data.value.len() < 2
-                        || tx
-                            .send(AppEvent::Bluetooth(BluetoothEvent::HrMessage(
-                                HeartrateMessage::new(&data.value),
-                            )))
-                            .is_err()
-                    {
-                        break;
-                    }
+        let mut notification_stream = cheststrap.notifications().await?;
+        let fut = tokio::spawn(async move {
+            while let Some(data) = notification_stream.next().await {
+                if data.value.len() < 2
+                    || tx
+                        .send(AppEvent::Bluetooth(BluetoothEvent::HrMessage(
+                            HeartrateMessage::new(&data.value),
+                        )))
+                        .is_err()
+                {
+                    break;
                 }
-                warn!("BT transceiver terminated");
-                Err(anyhow!("listener terminated"))
-            });
-            Ok(fut)
-        })
+            }
+            warn!("BT transceiver terminated");
+            Err(anyhow!("listener terminated"))
+        });
+        Ok(fut)
     }
 
     /// Launches a peripheral updater task.
@@ -199,123 +188,102 @@ impl BluetoothController {
     }
 }
 
+#[async_trait]
 impl BluetoothApi for BluetoothController {
-    fn select_peripheral<'a>(
-        &'a self,
-        uuid: &'a DeviceDescriptor,
-    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>> {
-        Box::pin(async move {
-            self.model.write().await.select_device(uuid.clone());
-            Ok(())
-        })
-    }
-
-    fn select_adapter<'a>(
-        &'a self,
-        adapter: &'a AdapterDescriptor,
-    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>> {
-        Box::pin(async move {
-            self.model
-                .write()
-                .await
-                .select_adapter(adapter.get_uuid())?;
-            Ok(())
-        })
-    }
-
     fn get_model(&self) -> Result<ModelHandle<dyn BluetoothModelApi>> {
         Ok(self.model.clone().into())
     }
 
-    fn discover_adapters<'a>(
-        &'a mut self,
-    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>> {
-        Box::pin(async move {
-            let manager = Manager::new().await?;
-            let adapters = manager.adapters().await?;
-            let mut model = Vec::new();
-            for adapter in adapters {
-                let name = adapter.adapter_info().await.unwrap_or("unknown".into());
-                let desc = AdapterDescriptor::new(name);
-                model.push(desc.clone());
-                self.adapters.insert(*desc.get_uuid(), adapter);
-            }
-            self.model.write().await.set_adapters(model);
+    async fn discover_adapters(&mut self) -> Result<()> {
+        let manager = Manager::new().await?;
+        let adapters = manager.adapters().await?;
+        let mut model = Vec::new();
+        for adapter in adapters {
+            let name = adapter.adapter_info().await.unwrap_or("unknown".into());
+            let desc = AdapterDescriptor::new(name);
+            model.push(desc.clone());
+            self.adapters.insert(*desc.get_uuid(), adapter);
+        }
+        self.model.write().await.set_adapters(model);
 
-            Ok(())
-        })
+        Ok(())
     }
 
-    fn start_scan<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>> {
-        Box::pin(async move {
-            if self.model.read().await.is_scanning() {
-                return Err(anyhow!("Already scanning"));
-            }
-            let handle = self.get_adapter().await?;
-            handle.start_scan(ScanFilter::default()).await?;
-            trace!(
-                "Scanning started on adapter {}.",
-                handle.adapter_info().await.unwrap_or("unknown".into())
-            );
-            if self.peri_updater_handle.is_none() {
-                self.peri_updater_handle = Some(
-                    self.launch_periphal_updater(handle.clone(), self.event_bus.clone())
-                        .await?,
-                );
-            }
-            Ok(())
-        })
+    async fn select_peripheral(&self, uuid: &DeviceDescriptor) -> Result<()> {
+        self.model.write().await.select_device(uuid.clone());
+        Ok(())
     }
 
-    fn stop_scan<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>> {
-        Box::pin(async move {
-            if !self.model.read().await.is_scanning() {
-                return Err(anyhow!("stop scan requested but no scan active"));
-            }
-            let handle = self.get_adapter().await?;
-            handle.stop_scan().await?;
-            trace!(
-                "Stopped scanning on adapter {}.",
-                handle.adapter_info().await?
-            );
-            if let Some(updater_handle) = &self.peri_updater_handle.take() {
-                updater_handle.abort();
-            }
-            Ok(())
-        })
+    async fn select_adapter(&self, adapter: &AdapterDescriptor) -> Result<()> {
+        self.model
+            .write()
+            .await
+            .select_adapter(adapter.get_uuid())?;
+        Ok(())
     }
 
-    fn start_listening<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>> {
-        Box::pin(async move {
-            let handle = self.get_adapter().await?;
-            if let Some(jh) = &self.listener_handle {
-                jh.abort();
-            }
-
-            let desc = self
-                .model
-                .write()
-                .await
-                .get_selected_device()
-                .as_ref()
-                .ok_or(anyhow!("no selected device!"))?
-                .clone();
-            self.listener_handle = Some(
-                Self::listen_to_peripheral(handle.clone(), desc.address, self.event_bus.clone())
+    async fn start_scan(&mut self) -> Result<()> {
+        if self.model.read().await.is_scanning() {
+            return Err(anyhow!("Already scanning"));
+        }
+        let handle = self.get_adapter().await?;
+        handle.start_scan(ScanFilter::default()).await?;
+        trace!(
+            "Scanning started on adapter {}.",
+            handle.adapter_info().await.unwrap_or("unknown".into())
+        );
+        if self.peri_updater_handle.is_none() {
+            self.peri_updater_handle = Some(
+                self.launch_periphal_updater(handle.clone(), self.event_bus.clone())
                     .await?,
             );
-            self.model.write().await.set_listening(Some(desc.address));
-            Ok(())
-        })
+        }
+        Ok(())
     }
 
-    fn stop_listening<'a>(&'a mut self) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'a>> {
-        Box::pin(async move {
-            if let Some(handle) = &self.listener_handle {
-                handle.abort();
-                self.model.write().await.set_listening(None);
-            }
-            Ok(())
-        })
+    async fn stop_scan(&mut self) -> Result<()> {
+        if !self.model.read().await.is_scanning() {
+            return Err(anyhow!("stop scan requested but no scan active"));
+        }
+        let handle = self.get_adapter().await?;
+        handle.stop_scan().await?;
+        trace!(
+            "Stopped scanning on adapter {}.",
+            handle.adapter_info().await?
+        );
+        if let Some(updater_handle) = &self.peri_updater_handle.take() {
+            updater_handle.abort();
+        }
+        Ok(())
+    }
+
+    async fn start_listening(&mut self) -> Result<()> {
+        let handle = self.get_adapter().await?;
+        if let Some(jh) = &self.listener_handle {
+            jh.abort();
+        }
+
+        let desc = self
+            .model
+            .write()
+            .await
+            .get_selected_device()
+            .as_ref()
+            .ok_or(anyhow!("no selected device!"))?
+            .clone();
+        self.listener_handle = Some(
+            Self::listen_to_peripheral(handle.clone(), desc.address, self.event_bus.clone())
+                .await?,
+        );
+        self.model.write().await.set_listening(Some(desc.address));
+        Ok(())
+    }
+
+    async fn stop_listening(&mut self) -> Result<()> {
+        if let Some(handle) = &self.listener_handle {
+            handle.abort();
+            self.model.write().await.set_listening(None);
+        }
+        Ok(())
     }
 }
